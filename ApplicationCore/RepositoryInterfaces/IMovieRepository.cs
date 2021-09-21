7315ac101b0c4729b28fb35970f74b68ab73e875
Sarah <qimengyuan1231@gmail.com>
2021-09-21 18:52:02 -0500

    descriptive comment for code changes

---
+++ ApplicationCore/RepositoryInterfaces/IMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using ApplicationCore.RepositoryInterfaces;
+
+namespace ApplicationCore.RepositoryInterfaces
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<Movie> Get30HighestGrossingMovies();
+        // select top 30 from movie order by revenue
+
+
+    }
+}
